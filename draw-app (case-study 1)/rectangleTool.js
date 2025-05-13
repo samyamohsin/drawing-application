e022d33d34a2e2edd4707047c2a6a8309c3727dc
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-13 07:28:02 +0100

    l

--- draw-app (case-study 1)/rectangleTool.js
+++ draw-app (case-study 1)/rectangleTool.js
@@ -1,4 +1,29 @@
 function rectangleTool () {
     this.name = "RectangleTool";
 	//this.icon = "";
+
+    var startMouseX = -1;
+    var startMouseY = -1;
+    var drawing  = false;
+
+    this.draw = function() {
+        if(mousePressed) {
+            if(startMouseX == -1) {
+                startMouseX = mouseX;
+                startMouseY = mouseY;
+                drawing = true;
+
+                loadPixel();
+            } else {
+                updatePixel();
+
+                rect(startMouseX, startMouseY, abs(mouseX - startMouseX), abs(mouseY - startMouseY) );
+            }
+        } else is (drawing) {
+            drawing = false;
+            startMouseX = -1;
+            startMouseY = -1;
+        }
+    }
+
 }
