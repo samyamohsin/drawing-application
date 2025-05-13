3c316e7885488e47c500ea45b8c3afc50d65f8db
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-13 20:31:33 +0100

    b

--- draw-app (case-study 1)/blurTool.js
+++ draw-app (case-study 1)/blurTool.js
@@ -1,3 +1,7 @@
 function blurTool() {
-    
+    this.name = "blur";
+    //https://www.flaticon.com/free-icon/shape_15631444?term=blur&page=1&position=9&origin=tag&related_id=15631444
+    this.icon = "assets/blurTool.png";
+
+
 }
