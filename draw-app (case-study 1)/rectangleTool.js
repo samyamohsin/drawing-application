8f8d7e1b659453a2a3dd3050ef600f07f7c9543b
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-13 19:59:13 +0100

    k

--- draw-app (case-study 1)/rectangleTool.js
+++ draw-app (case-study 1)/rectangleTool.js
@@ -3,12 +3,15 @@ function rectangleTool () {
     //https://www.flaticon.com/free-icon/rectangle_5895916 
 	this.icon = "assets/rectangle.png";
 
+    // this declares the starting a and y postion of the rectangle
     var startMouseX = -1;
     var startMouseY = -1;
     var drawing  = false;
 
     this.draw = function() {
+        //the if statement checks if the mouse has been clicked
         if(mouseIsPressed) {
+            // once the mouse has been clicked it set the starting position of the rectangle 
             if(startMouseX == -1) {
                 startMouseX = mouseX;
                 startMouseY = mouseY;
@@ -18,10 +21,7 @@ function rectangleTool () {
             } else {
                 updatePixels();
 
-                rect(startMouseX, 
-                    startMouseY, 
-                    abs(mouseX - startMouseX), 
-                    abs(mouseY - startMouseY) );
+                rect(startMouseX, startMouseY, abs(mouseX - startMouseX), abs(mouseY - startMouseY) );
             }
         } else if (drawing) {
             drawing = false;
