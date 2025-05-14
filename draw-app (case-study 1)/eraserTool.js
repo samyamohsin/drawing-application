d27036904f5407771877a0ed272959a9cb77e04e
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-14 06:02:03 +0100

    e

--- draw-app (case-study 1)/eraserTool.js
+++ draw-app (case-study 1)/eraserTool.js
@@ -1,7 +1,8 @@
 function eraserTool () {
-    this.name = "eraser";
     //https://www.flaticon.com/free-icon/eraser_4043845?term=eraser&page=1&position=1&origin=tag&related_id=4043845
     this.icon = "asssets/eraserTool.png";
+    this.name = "eraser";
+    
 
     var previousMouseX = -1;
 	var previousMouseY = -1;
