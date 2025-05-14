c28f9633a19e2aadf6c765cf2548928ebcd401b2
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-14 05:49:42 +0100

    b

--- draw-app (case-study 1)/blurTool.js
+++ draw-app (case-study 1)/blurTool.js
@@ -2,16 +2,4 @@ function blurTool() {
     this.name = "blur";
     //https://www.flaticon.com/free-icon/shape_15631444?term=blur&page=1&position=9&origin=tag&related_id=15631444
     this.icon = "assets/blurTool.png";
-
-    let drawMode = true; // true = draw, false = blur
-      let brushSize = 40;
-
-
-        // Add a button to toggle modes
-        let button = createButton('Toggle Draw/Blur');
-        button.position(10, height + 10);
-        button.mousePressed(() => {
-          drawMode = !drawMode;
-        });
-
 }
