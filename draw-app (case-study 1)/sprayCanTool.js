e1c1cba8096189e006073d02c88978d452f035ed
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-14 11:47:53 +0100

    Fix(freehandTool.js): fixed line tool bug

--- draw-app (case-study 1)/sprayCanTool.js
+++ draw-app (case-study 1)/sprayCanTool.js
@@ -6,6 +6,7 @@ function SprayCanTool() {
     this. spread =  10;
 
     this.draw = function() {
+        strokeWeight(1);
         //if the mouse is pressed paint on the canvas
         //spread describes how far to spread the paint from the mouse pointer
         //points holds how many pixels of paint for each mouse press.
