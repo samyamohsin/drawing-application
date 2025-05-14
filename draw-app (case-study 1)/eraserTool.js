e51d13d81dbd31078399bea1f368cb4951c33908
samya <smohs001@campus.goldsmith.ac.uk>
2025-05-14 05:55:05 +0100

    b

--- draw-app (case-study 1)/eraserTool.js
+++ draw-app (case-study 1)/eraserTool.js
@@ -1,4 +1,34 @@
 function eraserTool () {
     this.name = "eraser";
-    this.icon = "asssets/....";
+    //https://www.flaticon.com/free-icon/eraser_4043845?term=eraser&page=1&position=1&origin=tag&related_id=4043845
+    this.icon = "asssets/eraserTool.png";
+
+    var previousMouseX = -1;
+	var previousMouseY = -1;
+
+	this.draw = function(){
+		//if the mouse is pressed
+		if(mouseIsPressed){
+			//check if they previousX and Y are -1. set them to the current
+			//mouse X and Y if they are.
+			if (previousMouseX == -1){
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+			//if we already have values for previousX and Y we can draw a line from 
+			//there to the current mouse location
+			else{
+				line(previousMouseX, previousMouseY, mouseX, mouseY);
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+		}
+		//if the user has released the mouse we want to set the previousMouse values 
+		//back to -1.
+		//try and comment out these lines and see what happens!
+		else{
+			previousMouseX = -1;
+			previousMouseY = -1;
+		}
+	};
 }
