e1897f6195bb1750e4f9886bd1c63a2c203f020b
samyamohsin <smohs001@campus.goldsmiths.ac.uk>
2025-05-15 16:59:39 +0100

    n

--- draw-app (case-study 1)/eraserTool.js
+++ draw-app (case-study 1)/eraserTool.js
@@ -4,18 +4,14 @@ function eraserTool () {
     this.name = "eraser";
     this.color = null;
 
-    let size = 20;
-
     this.draw = function () {
         if(mouseIsPressed && mouseX < width) {
             if(keyIsDown(SHIFT)) {
                 erase();
-                //strokeWeight(size);
                 line(pmouseX, pmouseY, mouseX, mouseY);
                 noErase();
             } else {
                 stroke("white");
-                //strokeWeight(size);
                 line(pmouseX, pmouseY, mouseX, mouseY);
             }
         }
