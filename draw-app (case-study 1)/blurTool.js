d06df20657affd83609f69a692504d498d6e97e2
samyamohsin <smohs001@campus.goldsmiths.ac.uk>
2025-05-15 17:11:57 +0100

    h

--- draw-app (case-study 1)/blurTool.js
+++ draw-app (case-study 1)/blurTool.js
@@ -3,8 +3,6 @@ function blurTool() {
     //https://www.flaticon.com/free-icon/shape_15631444?term=blur&page=1&position=9&origin=tag&related_id=15631444
     this.icon = "assets/blurTool.png";
 
-
-    this.draw = function () {
-        
-    }
+	this.draw = function(){
+	};
 }
