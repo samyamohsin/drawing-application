a1d0cb54859831c69e17ee3ca12da6aaddee8180
RiaFacey <rface001@campus.goldsmiths.ac.uk>
2025-05-16 01:32:46 +0100

    b

--- draw-app (case-study 1)/paintBucketTool.js
+++ draw-app (case-study 1)/paintBucketTool.js
@@ -3,48 +3,45 @@ function paintBucketTool () {
     this.icon = "assets/paintBucket.png";
     this.name = "bucket";
 //when tool is clicked, the color selected can fill the background.
-    this.draw = function () {
-		if(mouseIsPressed) {
-     let c = get(mouseX,mouseY);
-	 loadPixels();
-	 let stack = [[mouseX,mouseY]];
-	 let targetColor = c;
-	let fillColor = color(colourP.selectedColour);
-	 let  v = new Set();
-	 
-	 	while(stack.length > 0){
+
+	this.draw = function(){
+		if(mouseIsPressed){
+			let c = get(mouseX, mouseY);
+			
+			loadPixels();
+			
+			let targetColor = c;
+			let fillColor = color(colourP.selectedColour);
+			
+			let stack = [[mouseX, mouseY]];
+			let visited = new Set();
+			
+			while(stack.length > 0){
 				let [x, y] = stack.pop();
-				let keys = `${x},${y}`;
-				 if(v.has(keys) || x<0 || x >= width || y<0 || y>=height) continue;
 				let key = `${x},${y}`;
-				 if(visited.has(key) || x<0 || x >= width || y<0 || y>=height) continue;
-
-
-
-let currentColor = get(x,y);
- if(!this.colorsMatch(currentColor, targetColor)) continue;
-
- visited.add(keys);
- v.add(key);
- set(x,y,fillColor);
-
- stack.push([x+1,y]);
- stack.push([x-1,y]);
- stack.push([x,y + 1]);
- stack.push([x,y - 1]);
-
+				
+				if(visited.has(key) || x < 0 || x >= width || y < 0 || y >= height) continue;
+				
+				let currentColor = get(x, y);
+				
+				if(!this.colorsMatch(currentColor, targetColor)) continue;
+				
+				visited.add(key);
+				
+				set(x, y, fillColor);
+				
+				stack.push([x+1, y]);
+				stack.push([x-1, y]);
+				stack.push([x, y+1]);
+				stack.push([x, y-1]);
+			}
+			
+			updatePixels();
 		}
-
-updatePixels();
-
-
+	};
 
 	this.colorsMatch = function(c1, c2) {
 		return c1[0] === c2[0] && c1[1] === c2[1] && c1[2] === c2[2];
+	};
+}
 
-	
-
-		}
-
-	}
-}}
